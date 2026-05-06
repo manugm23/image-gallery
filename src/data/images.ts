66fce44e2f360f79ef9fcca2ffdf56d5be551306
manugm23 <manugonzalezfreelancer@gmail.com>
2026-05-06 11:47:10 +0200

    feat: add personal images

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,14 +1,13 @@
 import type { Image } from '../types/image'
 
 export const INITIAL_IMAGES: Image[] = [
-  { id: '1', url: 'https://picsum.photos/id/10/600/400', alt: 'Nature landscape' },
-  { id: '2', url: 'https://picsum.photos/id/20/600/400', alt: 'Urban architecture' },
-  { id: '3', url: 'https://picsum.photos/id/30/600/400', alt: 'Wildlife' },
-  { id: '4', url: 'https://picsum.photos/id/40/600/400', alt: 'Snowy mountains' },
-  { id: '5', url: 'https://picsum.photos/id/50/600/400', alt: 'Tropical forest' },
-  { id: '6', url: 'https://picsum.photos/id/60/600/400', alt: 'Ocean at sunset' },
-  { id: '7', url: 'https://picsum.photos/id/70/600/400', alt: 'City at night' },
-  { id: '8', url: 'https://picsum.photos/id/80/600/400', alt: 'Flower field' },
-  { id: '9', url: 'https://picsum.photos/id/90/600/400', alt: 'River in the mountains' },
-  { id: '10', url: 'https://picsum.photos/id/100/600/400', alt: 'Forest path' },
+  { id: '1', url: '/bean-pictures/bean1.webp', alt: 'Bean 1' },
+  { id: '2', url: '/bean-pictures/bean2.webp', alt: 'Bean 2' },
+  { id: '3', url: '/bean-pictures/bean3.webp', alt: 'Bean 3' },
+  { id: '4', url: '/bean-pictures/bean4.webp', alt: 'Bean 4' },
+  { id: '5', url: '/bean-pictures/bean5.webp', alt: 'Bean 5' },
+  { id: '6', url: '/bean-pictures/bean6.webp', alt: 'Bean 6' },
+  { id: '7', url: '/bean-pictures/bean7.webp', alt: 'Bean 7' },
+  { id: '8', url: '/bean-pictures/bean8.webp', alt: 'Bean 8' },
+  { id: '9', url: '/bean-pictures/bean9.webp', alt: 'Bean 9' }
 ]
